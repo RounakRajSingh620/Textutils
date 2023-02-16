f20e47473d95e8b0150378d89c3ed7aa1038223b
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-02-16 19:15:33 +0530

    Basics of React_Js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       {/*<Navbar  title ="TextUtils" AboutText="About Us"/>*/}
-      <Navbar  title ="TextUtils" />
       {/*<Navbar />*/}
+      <Navbar title="TextUtils" />
+
+      <div className="container my-3">
+
+      <TextForm heading="Enter the text to analyze below" />
+      </div>
     </>);
 }
 
