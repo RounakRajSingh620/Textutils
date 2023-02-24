76f13233b6c110e01f79898b1b2199b02d50d01d
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-02-24 22:33:55 +0530

    TextUtils

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 
+import { useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <>
       {/*<Navbar  title ="TextUtils" AboutText="About Us"/>*/}
       {/*<Navbar />*/}
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={darkMode} />
       <div className="container my-3">
-       <TextForm heading="Enter the text to analyze below" />
-      {/*<About/>*/} 
+        <TextForm heading="Enter the text to analyze below" />
+        {/*<About/>*/}
       </div>
     </>);
 }
