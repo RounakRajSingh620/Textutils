d398a3562ca81405016d21b86b16b3caadba7604
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-02-27 22:38:13 +0530

    TextUtils

--- src/App.js
+++ src/App.js
@@ -8,11 +8,19 @@ import React, { useState } from 'react';
 
 function App() {
   const [mode, setMode] = useState('light');
+  const toggleMode =()=>{
+    if(mode === 'light'){
+      setMode('dark')
+    }
+    else{
+      setMode('light')
+    }
+  }
   return (
     <>
       {/*<Navbar  title ="TextUtils" AboutText="About Us"/>*/}
       {/*<Navbar />*/}
-      <Navbar title="TextUtils" mode={mode} />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" />
         {/*<About/>*/}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,6 +22,10 @@ export default function Navbar(props) {
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline" type="submit">Search</button>
           </form>*/}
+          <div className={`form-check form-switch text-${props.mode=== 'light'?'dark':'light'}`}>
+            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <label className="form-check-label" for="flexSwitchCheckDefault">Enable Dark Mode</label>
+          </div>
         </div>
       </div>
     </nav>
