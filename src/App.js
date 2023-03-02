094448d8be0b4c32dc07346420342961b5982c87
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-03-02 19:02:22 +0530

    TextUtils

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import Alert from './Alert';
 
 function App() {
   const [mode, setMode] = useState('light');
-  const toggleMode =()=>{
-    if(mode === 'light'){
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark')
-      document.body.style.backgroundColor='#042743';
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
     }
-    else{
+    else {
       setMode('light')
-      document.body.style.backgroundColor='white';
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
   return (
@@ -23,8 +36,9 @@ function App() {
       {/*<Navbar  title ="TextUtils" AboutText="About Us"/>*/}
       {/*<Navbar />*/}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" mode={mode}/>
+        <TextForm heading="Enter the text to analyze below" mode={mode} />
         {/*<About/>*/}
       </div>
     </>);
