aeb60a26f9230eda9e95fbba97cc62c894591746
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-03-05 19:16:41 +0530

    TextUtils

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 // import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -43,8 +49,8 @@ function App() {
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-        {/*<About/>*/}
+        {/*<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />*/}
+        <About/>
       </div>
     </>);
 }
