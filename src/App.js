aa506acbc5ab3c36856f461a37c56e0452a6aaa4
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-03-09 21:00:04 +0530

    TextUtils

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         <Alert alert={alert} />
         <div className="container my-3">
           <Routes>
-            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/about" element={<About mode={mode} />}></Route>
             <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />}>
             </Route>
           </Routes>
