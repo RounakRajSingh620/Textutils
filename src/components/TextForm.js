77f66db73010394f73bf1b61f45c41b61ca71087
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-16 16:16:21 +0530

    TextUtils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,18 +5,18 @@ export default function TextForm(props) {
         // console.log("Uppercase was clicked:  " + text);
         let newText = text.toUpperCase();
         setText(newText);
-        props.showAlert("Converted to uppercase!","success");
+        props.showAlert("Converted to uppercase!", "success");
     }
     const handleLoClick = () => {
         // console.log("Lowercase was clicked:  " + text);
         let newText = text.toLowerCase();
         setText(newText);
-        props.showAlert("Converted to lowercase!","success");
+        props.showAlert("Converted to lowercase!", "success");
     }
     const handleClearClick = () => {
         let newText = '';
         setText(newText);
-        props.showAlert("Text Cleared!","success");
+        props.showAlert("Text Cleared!", "success");
     }
 
     const handleonChange = (event) => {
@@ -27,13 +27,13 @@ export default function TextForm(props) {
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
-        props.showAlert("Text Copied!","success");
+        props.showAlert("Text Copied!", "success");
     }
 
     const handleExtraSpace = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "))
-        props.showAlert("Removed Extraspaces!","success");
+        props.showAlert("Removed Extraspaces!", "success");
     }
     const [text, setText] = useState('');
     // text="new text"; // Wrong way to change the state
@@ -45,18 +45,18 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleonChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase </button>
-                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-2" onClick={handleClearClick}>ClearText</button>
-                <button className="btn btn-primary mx-2" onClick={handleCopy}>CopyText</button>
-                <button className="btn btn-primary mx-2" onClick={handleExtraSpace}>Remove Extra Space</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Uppercase </button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>ClearText</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleCopy}>CopyText</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleExtraSpace}>Remove Extra Space</button>
             </div>
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h2>Your text summary</h2>
-                <p>{text.split(" ").length} words  and  {text.length} characters </p>
+                <p>{text.split(" ").filter((element) => { return element.length !==0 }).length} words  and  {text.length} characters </p>
                 <p>{0.008 * text.split(" ").length} Minutes read</p>
                 <h2>Preview</h2>
-                <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+                <p>{text.length > 0 ? text : "Enter something in the textbox above to preview it here"}</p>
             </div>
         </>
     )
