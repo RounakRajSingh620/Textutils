249b70a613624e3713706fbe7651b4b5b68087b9
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-19 20:49:46 +0530

    TextUtils

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,11 +27,10 @@ export default function Navbar(props) {
             <div className="bg-danger rounded mx-2" onClick={() => { props.toggleMode('danger') }} style={{ height: "30px", width: "30px", cursor: 'pointer' }}></div>
             <div className="bg-success rounded mx-2" onClick={() => { props.toggleMode('success') }} style={{ height: "30px", width: "30px", cursor: 'pointer' }}></div>
             <div className="bg-warning rounded mx-2" onClick={() => { props.toggleMode('warning') }} style={{ height: "30px", width: "30px", cursor: 'pointer' }}></div>
+            <div className="bg-light rounded mx-2" onClick={() => { props.toggleMode('light') }} style={{ height: "30px", width: "30px", cursor: 'pointer' }}></div>
+            <div className="bg-dark rounded mx-2" onClick={() => { props.toggleMode('dark') }} style={{ height: "30px", width: "30px", cursor: 'pointer' }}></div>
           </div>
-          <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
-            <input className="form-check-input" onClick={() => { props.toggleMode('null') }} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
-          </div>
+
         </div>
       </div>
     </nav>
